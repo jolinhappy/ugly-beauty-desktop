9d3cf1b3aed1d6f5b0a7b61103c77f2885f1439a
Phoebe <jolinhappy1990@hotmail.com>
2020-11-27 21:05:00 +0800

    feat: add start menu list

--- src/App.vue
+++ src/App.vue
@@ -15,8 +15,11 @@
   --blood-red: #960820;
   --light-red: #ff8a9e;
   --dark-red: #4d000d;
+  --middle-red: #6a1120;
   --white: #fff;
   --text-black: #252223;
+  height: 100%;
+  width: 100%;
   background-image: url(https://i2.wp.com/www.jolinjenerationgallery.com/albums/userpics/10001/Cai20Yi20Lin20_Jolin_Tsai_3C3CZi20Shuo20_Ugly_Beauty3E3E20_05.jpg);
   background-size: cover;
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="desktop">
-    <div class="desktop-app-section">
+    <div class="desktop-app-section" @click="menuClose">
       <div class="desktop-app-group">
         <div class="desktop-app">
           <div class="desktop-app-icon">
@@ -47,7 +47,7 @@
         <div class="desktop-app">
           <div class="desktop-app-icon">
             <img
-              src="https://picsum.photos/40/40/?random=7"
+              src="https://picsum.photos/40/40/?random=17"
               alt="app-icon"
               class="icon-img"
             />
@@ -56,9 +56,8 @@
         </div>
       </div>
     </div>
-
     <div class="desktop-footer">
-      <div class="footer-left">
+      <div class="footer-left" @click="clickStart" :class="{ active }">
         <div class="footer-start-icon">
           <img
             src="https://picsum.photos/20/20/?random=30"
@@ -67,7 +66,58 @@
         </div>
         <div class="footer-start-text">start</div>
       </div>
-
+      <div class="footer-menu" v-show="menuDisplay">
+        <div class="menu-item">
+          <div class="menu-item-icon">
+            <img
+              src="https://picsum.photos/20/20/?random=1"
+              alt="menu-icon"
+              class="menu-icon-img"
+            />
+          </div>
+          <div class="menu-item-title">音樂播放器</div>
+        </div>
+        <div class="menu-item">
+          <div class="menu-item-icon">
+            <img
+              src="https://picsum.photos/20/20/?random=4"
+              alt="menu-icon"
+              class="menu-icon-img"
+            />
+          </div>
+          <div class="menu-item-title">Jolin大紀事</div>
+        </div>
+        <div class="menu-item">
+          <div class="menu-item-icon">
+            <img
+              src="https://picsum.photos/20/20/?random=17"
+              alt="menu-icon"
+              class="menu-icon-img"
+            />
+          </div>
+          <div class="menu-item-title">蔡依林</div>
+        </div>
+        <div class="menu-item">
+          <div class="menu-item-icon">
+            <img
+              src="https://picsum.photos/20/20/?random=24"
+              alt="menu-icon"
+              class="menu-icon-img"
+            />
+          </div>
+          <div class="menu-item-title">網路上的芳鄰</div>
+        </div>
+        <div class="menu-item">
+          <div class="menu-item-icon">
+            <img
+              src="https://picsum.photos/20/20/?random=18"
+              alt="menu-icon"
+              class="menu-icon-img"
+            />
+          </div>
+          <div class="menu-item-title">我的電腦</div>
+        </div>
+      </div>
       <div class="footer-right">
         <div class="footer-skin-change">變更桌面</div>
         <div class="footer-time">12:35 PM</div>
@@ -79,13 +129,36 @@
 <script>
 export default {
   name: "Home",
+  data() {
+    return {
+      active: false,
+      menuDisplay: false,
+    };
+  },
+  methods: {
+    clickStart() {
+      this.active = !this.active;
+      this.menuDisplay = !this.menuDisplay;
+    },
+    menuClose() {
+      if (this.menuDisplay) {
+        this.menuDisplay = !this.menuDisplay;
+        this.active = !this.active;
+      } else return;
+    },
+  },
 };
 </script>
 
 <style scoped>
+.desktop {
+  width: 100%;
+  height: 100%;
+}
 .desktop-app-section {
   display: flex;
   margin: 50px 40px;
+  height: 90vh;
 }
 .desktop-app-group {
   margin-right: 20px;
@@ -115,6 +188,7 @@ export default {
   align-items: center;
   color: var(--white);
   border-top: 1.5px solid var(--light-red);
+  z-index: 1;
 }
 
 .footer-left {
@@ -128,6 +202,7 @@ export default {
   border-left: 1.5px solid var(--light-red);
   border-right: 1.5px solid var(--dark-red);
   border-bottom: 1.5px solid var(--dark-red);
+  cursor: default;
 }
 .footer-start-icon {
   margin-top: 4px;
@@ -135,6 +210,42 @@ export default {
 .footer-start-text {
   margin-left: 8px;
   font-size: 20px;
+}
+.active {
+  border-top: 1.5px solid var(--dark-red);
+  border-left: 1.5px solid var(--dark-red);
+  border-right: 1.5px solid var(--light-red);
+  border-bottom: 1.5px solid var(--light-red);
+}
+.footer-menu {
+  position: absolute;
+  bottom: 45.7px;
+  width: 280px;
+  height: 300px;
+  background: var(--blood-red);
+  border-top: 2px solid var(--light-red);
+  border-right: 2px solid var(--middle-red);
+  border-bottom: 2px solid var(--middle-red);
+  z-index: 5;
+}
+.menu-item {
+  width: 280px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  cursor: default;
+}
+.menu-item:hover {
+  background: pink;
+  color: var(--blood-red);
+}
+.menu-item-icon {
+  margin-top: 2px;
+  margin-left: 30px;
+}
+.menu-item-title {
+  line-height: 70px;
+  margin-left: 15px;
 }
 
 .footer-right {
